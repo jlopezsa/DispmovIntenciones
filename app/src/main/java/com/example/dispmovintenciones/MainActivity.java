3d4bf50c92875f2acc2f37d9e4a82ab0d16e219e
Julian Lopez <jlopezsa@gmail.com>
2021-11-17 11:12:36 -0500

    Intencion para enviar mensajes por defecto a traves de Whatsapp

--- app/src/main/java/com/example/dispmovintenciones/MainActivity.java
+++ app/src/main/java/com/example/dispmovintenciones/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.dispmovintenciones;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private Button boton_mapa;
     private Button boton_foto;
     private Button boton_correo;
+    private Button boton_whatsapp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         boton_mapa = (Button) findViewById(R.id.btn_maps);
         boton_foto = findViewById(R.id.btn_foto);
         boton_correo = findViewById(R.id.btn_correo);
+        boton_whatsapp = findViewById(R.id.btn_whatsapp);
 
         boton_pagina.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 enviarCorreo(null);
+            }
+        });
+
+        boton_whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enviarMensajeWhatsapp(null);
             }
         });
     }
@@ -104,5 +119,30 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_TEXT,"texto  del correo");
         intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"xyz_123@gmail.com"});
         startActivity(intent);
+    }
+
+    private void enviarMensajeWhatsapp(View view){
+        Intent intent = new Intent("Android.intent.action.MAIN");
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setPackage("com.whatsapp");
+        intent.setType("text/plain");
+        intent.putExtra("jid", "57##########" + "@s.whatsapp.net");// 57 es el código del pais
+        intent.putExtra(Intent.EXTRA_TEXT, "Ejemplo de menseja enviado por defecto:\n"+
+                "\t\tDirección: Calle ##, ##, ## \n"+"\t\tTelefono: ######...\n"+
+                "\t\tNombre: Julián Perez Rodriuez\n"+
+                "======= Pedido =======\n"+
+                "id=100 - Insectoide: " + String.valueOf(10) + " productos\n"+
+                "id=103 - Tierrafertil: " + String.valueOf(5) + " productos\n");
+
+        try {
+            startActivity(intent);
+        } catch (android.content.ActivityNotFoundException ex) {
+            ex.printStackTrace();
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+            alerta.setMessage("Whatsapp no se encuentra instalado");
+            AlertDialog titulo = alerta.create();
+            titulo.setTitle("Cuidado");
+            titulo.show();
+        }
     }
 }
